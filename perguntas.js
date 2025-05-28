5618c057e105bafecc1075f35e170a4ebf229b7b
Phellipe Tscha <146093247+phellipetscha@users.noreply.github.com>
2025-05-28 09:29:24 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Filme',
-    'Quem é ator principal do filme homem de ferro três',
+    'Quem é ator principal do filme homem de ferro três?',
     'Robert Downey Jr.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital do egito',
+    'Qual a capital do egito?',
     'Cairo'
 )
 
 criaCartao(
     'Futebol',
-    'Quem é o melhor atacante do Brasil nos tempos de hoje',
+    'Quem é o melhor atacante do Brasil nos tempos de hoje?',
     'Vinícius Júnior'
 )
 
 criaCartao(
     'Formula 1',
-    'Qual é o melhor corredor da liga hoje em dia',
+    'Qual é o melhor corredor da liga hoje em dia?',
     'Max Verstappen'
 )
